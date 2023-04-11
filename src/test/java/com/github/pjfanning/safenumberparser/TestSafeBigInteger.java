807a1e3fa1d4babd5e3c505fe592a79112c58271
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-11 19:45:44 +0200

    simplify test code

--- src/test/java/com/github/pjfanning/safenumberparser/TestSafeBigInteger.java
+++ src/test/java/com/github/pjfanning/safenumberparser/TestSafeBigInteger.java
@@ -4,14 +4,10 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TestSafeBigInteger {
@@ -48,12 +44,6 @@ class TestSafeBigInteger {
     }
 
     private Config createTestConfig() throws URISyntaxException {
-        final ClassLoader parent = TestClassLoader.class.getClassLoader();
-        URL directoryUrl = parent.getResource("bigint-e-allowed");
-        assertNotNull(directoryUrl, "directory resource found");
-        File directory = Paths.get(directoryUrl.toURI()).toFile();
-        ClassLoaderWithResourcePath classLoader = new ClassLoaderWithResourcePath(
-                TestSafeBigInteger.class.getClassLoader(), directory);
-        return ConfigFactory.defaultApplication(classLoader);
+        return ConfigFactory.load("application-bigint-e-notation.conf");
     }
 }
