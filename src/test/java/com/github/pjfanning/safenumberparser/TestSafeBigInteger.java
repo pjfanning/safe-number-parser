81fae2adfc33e16c8d1a1b2fa3f06d243952611c
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-12 11:58:09 +0200

    more tests

--- src/test/java/com/github/pjfanning/safenumberparser/TestSafeBigInteger.java
+++ src/test/java/com/github/pjfanning/safenumberparser/TestSafeBigInteger.java
@@ -5,7 +5,7 @@ import com.typesafe.config.ConfigFactory;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.net.URISyntaxException;
+import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,14 +13,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class TestSafeBigInteger {
 
     @Test
-    void testExponentWithDefaultConfig() {
+    void testExponentValue() {
         String num = "1e10";
         assertThrows(NumberFormatException.class, () -> new SafeBigInteger(num));
     }
 
     @Test
-    void testBigExponent() throws Exception {
-        Config config = createTestConfig();
+    void testNonInteger() {
+        String num = "0.123";
+        assertThrows(NumberFormatException.class, () -> new SafeBigInteger(num));
+    }
+
+    @Test
+    void testBigExponentWithENotationAllowed() throws Exception {
+        Config config = ConfigFactory.load("application-bigint-e-notation.conf");
         try {
             SafeNumberParserConfig.setConfig(config);
             String num = "2e308";
@@ -32,8 +38,8 @@ class TestSafeBigInteger {
     }
 
     @Test
-    void testVeryBigExponent() throws Exception {
-        Config config = createTestConfig();
+    void testVeryBigExponentWithENotationAllowed() throws Exception {
+        Config config = ConfigFactory.load("application-bigint-e-notation.conf");
         try {
             SafeNumberParserConfig.setConfig(config);
             String num = "1e1010";
@@ -43,7 +49,29 @@ class TestSafeBigInteger {
         }
     }
 
-    private Config createTestConfig() throws URISyntaxException {
-        return ConfigFactory.load("application-bigint-e-notation.conf");
+    @Test
+    void testNonIntegerWithENotationAllowed() throws Exception {
+        Config config = ConfigFactory.load("application-bigint-e-notation.conf");
+        try {
+            SafeNumberParserConfig.setConfig(config);
+            String num = "0.123";
+            assertThrows(ArithmeticException.class, () -> new SafeBigInteger(num));
+        } finally {
+            SafeNumberParserConfig.setConfig(null);
+        }
     }
+
+    @Test
+    void testNonIntegerWithENotationAndInexactAllowed() throws Exception {
+        Config config = ConfigFactory.load("application-bigint-e-notation-inexact.conf");
+        try {
+            SafeNumberParserConfig.setConfig(config);
+            String num = "0.123";
+            SafeBigInteger sbi = new SafeBigInteger(num);
+            assertEquals(BigInteger.ZERO, sbi.toBigInteger());
+        } finally {
+            SafeNumberParserConfig.setConfig(null);
+        }
+    }
+
 }
