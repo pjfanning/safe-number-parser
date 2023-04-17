c6c23e7f82189573cc744ecd8d92d56384fdb966
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-17 22:08:29 +0200

    Create TestSafeBigDecimal.java

---
+++ src/test/java/com/github/pjfanning/safenumberparser/TestSafeBigDecimal.java
@@ -0,0 +1,41 @@
+package com.github.pjfanning.safenumberparser;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TestSafeBigDecimal {
+
+    @Test
+    void testValidExponentValue() throws ConstraintException {
+        String num = "1e1000";
+        assertEquals(new BigDecimal(num), new SafeBigDecimal(num).toBigDecimal());
+    }
+
+    @Test
+    void testValidNegativeExponentValue() throws ConstraintException {
+        String num = "-1e1000";
+        assertEquals(new BigDecimal(num), new SafeBigDecimal(num).toBigDecimal());
+    }
+
+    @Test
+    void testBigExponentValue() {
+        String num = "1e1001";
+        assertThrows(ConstraintException.class, () -> new SafeBigDecimal(num));
+    }
+
+    @Test
+    void testBigNegativeExponentValue() {
+        String num = "1e-1001";
+        assertThrows(ConstraintException.class, () -> new SafeBigDecimal(num));
+    }
+
+    @Test
+    void testNonInteger() throws ConstraintException {
+        String num = "0.123";
+        assertEquals(new BigDecimal(num), new SafeBigDecimal(num).toBigDecimal());
+    }
+}
