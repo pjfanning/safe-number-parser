fdac535e417fc4d40b3cf96876b5364daa88884a
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-20 14:57:03 +0200

    float tests

---
+++ src/test/java/com/github/pjfanning/safenumberparser/TestSafeFloat.java
@@ -0,0 +1,35 @@
+package com.github.pjfanning.safenumberparser;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TestSafeFloat {
+
+    @Test
+    void testNonInteger() throws ConstraintException {
+        String num = "1234567890.123456789";
+        assertEquals(new Float(num), new SafeFloat(num).toFloat());
+    }
+
+    @Test
+    void testMultiCharInteger() {
+        String num = TestUtils.genLargeNumber();
+        assertThrows(ConstraintException.class, () -> new SafeFloat(num));
+    }
+
+    @Test
+    void testConfigWithSmallLengthLimit() {
+        Config config = ConfigFactory.load("application-float-low-size-limit.conf");
+        try {
+            SafeNumberParserConfig.setConfig(config);
+            String num = TestUtils.genLargeNumber();
+            assertThrows(ConstraintException.class, () -> new SafeFloat(num));
+        } finally {
+            SafeNumberParserConfig.setConfig(null);
+        }
+    }
+}
